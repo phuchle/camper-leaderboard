7f9bbc141e32d48ddfa414d33fdad75b96dc2d3d
Phuc Le <le.phuc.h@gmail.com>
2017-05-15 13:39:28 -0700

    First commit.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let webpack = require('webpack');
+
+let config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ]}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
+
+module.exports = config;
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
